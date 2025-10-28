7696b58d116e2e60b905681e61163e83957136cd
saladimanojna315-maker <saladimanojna315@gmail.com>
2025-10-28 20:55:56 +0530

    Add files via upload

---
+++ Dragon Fruit.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int X;
+    scanf("%d", &X);
+    if(X % 2 == 0 && X>2)
+    {
+        printf("YES
+");
+    }
+    else
+    {
+        printf("NO
+");
+    }
+    return 0;
+}
