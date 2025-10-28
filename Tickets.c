ce77984fd94aa9faf3bb2f27946af13137b63c65
saladimanojna315-maker <saladimanojna315@gmail.com>
2025-10-28 20:21:50 +0530

    Add files via upload

---
+++ Tickets.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int X,Y;
+    scanf("%d",&X);
+    Y=4*X;
+    if(Y<=1000)
+    {
+        printf("YES
+");
+    }
+    else
+    {
+        printf("NO
+");
+    }
+    return 0;
+}
