76a4ecc6f9036655a9a5fa66c5321d2f75f0bcb5
saladimanojna315-maker <saladimanojna315@gmail.com>
2025-10-28 20:19:22 +0530

    Add files via upload

---
+++ Reach Home.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int X,Y;
+    int A;
+    scanf("%d%d",&X,&Y);
+    A=X*5;
+    if(A>=Y)
+    {
+        printf("YES
+");
+    }
+    else
+    {
+        printf("NO
+");
+    }
+    return 0;
+}
