e448cebc9f3797c8674c2ec89085c88b4d761f49
saladimanojna315-maker <saladimanojna315@gmail.com>
2025-10-28 20:20:31 +0530

    Add files via upload

---
+++ Subscription.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int X;
+    scanf("%d",&X);
+    if(X>40)
+    {
+        printf("YES
+");
+    }
+    else
+    {
+        printf("NO
+");
+    }
+    return 0;
+}
