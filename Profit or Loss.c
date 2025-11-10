dfe5ec1c25af6667533e1cd0979fac47ac28d90e
saladimanojna315-maker <saladimanojna315@gmail.com>
2025-11-10 21:09:36 +0530

    Add files via upload

---
+++ Profit or Loss.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int cost_price;
+    int selling_price;
+    scanf("%d", &cost_price);
+    scanf("%d", &selling_price);
+    if(selling_price > cost_price)
+    printf("Profit
+");
+    else if(cost_price > selling_price)
+    printf("Loss
+");
+    else
+    printf("No Profit and No Loss
+");
+    return 0;
+}
