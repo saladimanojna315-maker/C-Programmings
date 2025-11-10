eae5cf15028b34259a1957e452f6c7470c9cf69e
saladimanojna315-maker <saladimanojna315@gmail.com>
2025-11-10 21:09:03 +0530

    Add files via upload

---
+++ Contest.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int X, A, B;
+    int Y;
+    if(scanf("%d %d %d", &X, &A, &B)!=3);
+    Y=((A*1) + (B*2));
+    if( Y >= X)
+    printf("Qualify
+");
+    else
+    printf("Not Qualify
+");
+    return 0;
+}
