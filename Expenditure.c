f2a306c3795f3117e7587a8f9f0ce0e4950a8064
saladimanojna315-maker <saladimanojna315@gmail.com>
2025-11-10 21:04:32 +0530

    Add files via upload

---
+++ Expenditure.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int X;
+    int Y;
+    int Z;
+    scanf("%d", &X);
+    scanf("%d", &Y);
+    Z = Y * 30;
+    if(X >= Z)
+    printf("YES
+");
+    else
+    printf("NO
+");
+    return 0;
+}
