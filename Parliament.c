d822b33a1690dcf81c26022a624721667b04abd0
saladimanojna315-maker <saladimanojna315@gmail.com>
2025-11-14 23:12:49 +0530

    Add files via upload

---
+++ Parliament.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int N, X;
+    scanf("%d %d", &N, &X);
+    if(2 * X >= N )
+    {
+        printf("YES
+");
+    }
+    else
+    {
+        printf("NO
+");
+    }
+    return 0;
+}
