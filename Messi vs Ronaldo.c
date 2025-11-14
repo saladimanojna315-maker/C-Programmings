6e701347c1640079d336e32e5a9f8d5cdac2418d
saladimanojna315-maker <saladimanojna315@gmail.com>
2025-11-14 23:07:08 +0530

    Add files via upload

---
+++ Messi vs Ronaldo.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int A, B, X, Y;
+    int Messi, Ronaldo;
+    if(scanf("%d %d %d %d", &A, &B, &X, &Y)!=4);
+    Messi = (A * 2 + B * 1);
+    Ronaldo = (X * 2 + Y * 1);
+    if(Messi > Ronaldo)
+    printf("Messi
+");
+    else if(Ronaldo > Messi)
+    printf("Ronaldo
+");
+    else
+    printf("Equal
+");
+    return 0;
+}
