311a8740d328b6c205c16f9bf93977764c0de8a6
saladimanojna315-maker <saladimanojna315@gmail.com>
2025-11-14 23:12:02 +0530

    Add files via upload

---
+++ A to B.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() 
+{
+    int A, B, i;
+    if(scanf("%d %d", &A, &B)!=2);
+    for(i = A; i <= B; i++)
+    {
+    printf("%d ", i);
+    {
+        if(i < B)
+        printf("");
+    }
+    }
+    printf("\t");
+    return 0;
+}
