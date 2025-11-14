3490659d4d1c59ca8cb598367ce96d7fe3482541
saladimanojna315-maker <saladimanojna315@gmail.com>
2025-11-14 23:13:26 +0530

    Add files via upload

---
+++ sum of factors.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int N , i, sum = 0;
+    scanf("%d", &N);
+    for(i = 1; i <= N/2; i++)
+    {
+    if(N % i == 0)
+    sum = sum + i;
+    }
+    printf("%d
+",sum);
+    return 0;
+}
